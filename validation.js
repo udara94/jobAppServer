34113176b594a80e9447716c06521560e779b4ef
udara94 <37542994+udara94@users.noreply.github.com>
2020-03-16 21:26:55 +0530

    register api changes

--- validation.js
+++ validation.js
@@ -5,17 +5,9 @@ const Joi = require('@hapi/joi');
 //Register validation
 const registerValidation = data =>{
     const schema = Joi.object({
-        name: Joi.string()
-                .min(6)
+        mobile: Joi.string()
+                .min(10)
                 .required(),
-        email: Joi.string()
-                .min(6)
-                .required()
-                .email(),
-        password: Joi.string()
-                .min(6)
-                .required()
-    
     });
     return schema.validate(data);
 };
